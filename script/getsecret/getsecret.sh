9c64e1453ef42b8c7d8f1dea25f9ad63841dcd09
Mike Møller Nielsen <mmni@protonmail.com>
2020-04-02 19:00:32 +0200

    get secret with version

--- script/getsecret/getsecret.sh
+++ script/getsecret/getsecret.sh
@@ -2,7 +2,6 @@
 
 cd $(dirname $0)
 
-#curl --header "X-Vault-Token: myroot" \
-curl --header "X-Vault-Token: s.U8uzJYpgQUzP9Ol7ZczUQujy" \
+curl --header "X-Vault-Token: myroot" \
 --request GET \
-http://localhost:8200/v1/myengine/data/myspringapplication/staging
+"http://localhost:8200/v1/myengine/data/myspringapplication/staging?version=1"
--- script/postsecret/postsecret.sh
+++ script/postsecret/postsecret.sh
@@ -2,6 +2,5 @@
 cd $(dirname $0)
 
 curl --header "X-Vault-Token: myroot" \
---request POST \
 --data @newsecretpayload.json \
 http://localhost:8200/v1/myengine/data/myspringapplication/staging
